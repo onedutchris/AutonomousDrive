83fe2ce58b6a8178232216fcd7858b39da315751
cameronfr <catechboy@hotmail.com>
2014-09-09 16:18:34 +0900

    skeleton project

---
+++ src/auto.h
@@ -0,0 +1,20 @@
+/*
+ * auto.h
+ *
+ *  Created on: Sep 8, 2014
+ *      Author: cameronfranz
+ */
+
+#ifndef AUTO_H_
+#define AUTO_H_
+
+//turn
+void turn(int speed);
+
+//get data from environment
+void sense();
+
+//move robot
+void move();
+
+#endif /* AUTO_H_ */
---
+++ src/shared.h
@@ -0,0 +1,21 @@
+/*
+ * shared.h
+ *
+ *  Created on: Sep 9, 2014
+ *      Author: cameronfranz
+ */
+
+#ifndef SHARED_H_
+#define SHARED_H_
+
+#define LEFT_MOTOR_PORT 10
+#define RIGHT_MOTOR_PORT 1
+#define ARM_MOTOR_PORT 7
+#define CLAW_MOTOR_PORT 6
+
+#define LEFT_SONAR_PORT 5
+#define RIGHT_SONAR_PORT 7
+#define GYRO_PORT 2
+
+
+#endif /* SHARED_H_ */
