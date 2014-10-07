65f95f8b5a96293080b31222f17fa7a3e0225d63
cameronfr <catechboy@hotmail.com>
2014-10-07 16:13:37 +0900

    added some particle filter methods

--- src/auto.c
+++ src/auto.c
@@ -28,14 +28,14 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * Purdue Robotics OS contains FreeRTOS (http://www.freertos.org) whose source code may be
+ * Purdue Particleics OS contains FreeRTOS (http://www.freertos.org) whose source code may be
  * obtained from http://sourceforge.net/projects/freertos/files/ or on request.
  */
 /*
  * Runs the user autonomous code. This function will be started in its own task with the default
- * priority and stack size whenever the robot is enabled via the Field Management System or the
- * VEX Competition Switch in the autonomous mode. If the robot is disabled or communications is
- * lost, the autonomous task will be stopped by the kernel. Re-enabling the robot will restart
+ * priority and stack size whenever the Particle is enabled via the Field Management System or the
+ * VEX Competition Switch in the autonomous mode. If the Particle is disabled or communications is
+ * lost, the autonomous task will be stopped by the kernel. Re-enabling the Particle will restart
  * the task, not re-start it from where it left off.
  *
  * Code running in the autonomous task cannot access information from the VEX Joystick. However,
@@ -43,13 +43,12 @@
  * available, and it can access joystick information if called in this way.
  *
  * The autonomous task may exit, unlike operatorControl() which should never exit. If it does
- * so, the robot will await a switch to another mode or disable/enable cycle.
+ * so, the Particle will await a switch to another mode or disable/enable cycle.
  */
 #include "main.h"
 #include "shared.h"
 #include "auto.h"
 #include <Math.h>
-#include "robot.h"
 
 //custom data types
 struct cube {
@@ -65,6 +64,13 @@ typedef enum {
 	SEARCHING, DEPOSITING
 } STATE;
 
+struct Particle {
+	int x;
+	int y;
+	float heading;
+	float weight;
+};
+
 
 //external sensors
 extern Ultrasonic leftSonar;
@@ -73,85 +79,120 @@ extern Gyro gyro;
 
 STATE currentState;
 
-//sensor noise
+//sensor cosntants
 #define ULTRASONIC_NOISE 0.5
 #define ENCODER_NOISE 0.5
 #define LINE_TRACKER_NOISE 0.5
 #define POTENTIOMETER_NOISE 0.5
+
+//movement constants
+#define WHEEL_RADIUS
 
 //map data - no grid map because that's too large
 #define NUM_LINES 8
 #define NUM_CUBES 10
 const struct cube cubes[NUM_CUBES];
 const struct line lines[NUM_LINES];
+#define MAP_SIZE 100
 
-//particles
+//Particles
 #define NUM_PARTICLES 100
-struct Robot particles[NUM_PARTICLES];
+struct Particle * particles;
 
-//sensor and motor data
+/*//sensor and motor data
 int leftSonarValue;
 int rightSonarValue;
 
 int leftMotorValue;
 int rightMotorValue;
+*/
 
-//function definitions
-void sense();
-void update();
-void move(int speed, int direction, int turnAngle);
-void setMotors();
+//filter parameters
 
 
 //implementations
 void Auto_init() {
-	printf("initing");
-	//initialize particles
-	for (int i = 0; i<NUM_PARTICLES; i++) {
-		Robot_init(&particles[i], 100);
-	}
+	//initialize Particles
+	particles = init_particles();
 }
 
 void autonomous() {
-	//called every 20 ms
-	leftMotorValue = 0;
-	rightMotorValue = 0;
+
+	//update movement values
+	//calculateMovement();
+
+	//move();
+	//update sensor values
 	sense();
-	update();
+
+	//update_filter();
 }
 
 void move(int speed, int direction, int turnAngle) {
 	int turnMagnitude = -((abs(turnAngle)-45)/45);
 	if (turnAngle > 0) {
-		leftMotorValue  = (int)(direction * speed);
-		rightMotorValue = (int)(direction * speed * turnMagnitude);
+//		leftMotorValue  = (int)(direction * speed);
+//		rightMotorValue = (int)(direction * speed * turnMagnitude);
 	}
 	else if (turnAngle <= 0){
-		leftMotorValue  = (int)(direction * speed * turnMagnitude);
-		rightMotorValue = (int)(direction * speed);
+//		leftMotorValue  = (int)(direction * speed * turnMagnitude);
+//		rightMotorValue = (int)(direction * speed);
 	}
 }
 
-void update() {
+void update_filter() {
+
+	//update the weights
+	//weights = mes_prob_particles(Particles);
+
+	//move Particles
+	//Particles = convolute_particles(Particles);
+
+	//resample Particles
+	//Particles = resample_particles(Particles);
 
 	setMotors();
 }
 
 void sense() {
-	leftSonarValue = ultrasonicGet(leftSonar);
-	rightSonarValue = ultrasonicGet(rightSonar);
-	if (leftSonarValue == 0) {
-		leftSonarValue = 300;
-	}
-	if (rightSonarValue == 0) {
-			rightSonarValue = 300;
+	//leftSonarValue = ultrasonicGet(leftSonar);
+	//rightSonarValue = ultrasonicGet(rightSonar);
+
+	//if (leftSonarValue == 0) {
+		//leftSonarValue = 300;
+	//}
+	//if (rightSonarValue == 0) {
+	//		rightSonarValue = 300;
 		}
 }
 
 void setMotors() {
-	motorSet(LEFT_MOTOR_1_PORT, leftMotorValue);
-	motorSet(LEFT_MOTOR_2_PORT, leftMotorValue);
-	motorSet(RIGHT_MOTOR_1_PORT, -rightMotorValue);
-	motorSet(RIGHT_MOTOR_2_PORT, -rightMotorValue);
+	//motorSet(LEFT_MOTOR_1_PORT, leftMotorValue);
+	//motorSet(LEFT_MOTOR_2_PORT, leftMotorValue);
+	//motorSet(RIGHT_MOTOR_1_PORT, -rightMotorValue);
+	//motorSet(RIGHT_MOTOR_2_PORT, -rightMotorValue);
 }
-//auto.c
+
+//*****PARTICLE FILTER METHODS******//
+
+struct Particle * init_particles() {
+	struct Particle p[NUM_PARTICLES];
+	for (int i = 0; i<NUM_PARTICLES; i++) {
+			p[i].x = (int) floor(((float)rand()/RAND_MAX)*MAP_SIZE);
+			p[i].y = (int) floor(((float)rand()/RAND_MAX)*MAP_SIZE);
+			p[i].heading = ((float)rand()/RAND_MAX) * 2 * 3.14;
+	}
+	return p;
+}
+
+//TODO: Fix motion model
+struct Particle * move_particles(struct Particle p[], int speed, int direction, int turnAngle) {
+
+}
+
+float mes_prob_particles(struct Particle Particle) {
+
+}
+void Particle_setNoise(struct Particle * Particle, float sonar_noise, float move_noise,float gyro_noise) {
+
+};
--- src/auto.h
+++ src/auto.h
@@ -8,12 +8,15 @@
 #ifndef AUTO_H_
 #define AUTO_H_
 
-struct Robot {
-	int x;
-	int y;
-	float heading;
-};
-
+void sense();
+void update_filter();
+void move(int speed, int direction, int turnAngle);
+void setMotors();
+void calculateMovement();
+struct Particle * init_particles();
+struct Particle * move_particles(struct Particle Particle, int speed, int direction, int turnAngle); //convolute Particles
+float mes_prob_particles(struct Particle Particle); //get measurement probability of Particle
+void set_noise_particles(struct Particle * Particle, float sonar_noise, float move_noise, float gyro_noise);
 void Auto_init();
 
 #endif /* AUTO_H_ */
