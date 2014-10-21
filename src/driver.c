68e85def1ae155d593c0fca2568900c103d09fdd
cameronfr <catechboy@hotmail.com>
2014-10-21 16:24:48 +0900

    improved on angle correction

--- src/driver.c
+++ src/driver.c
@@ -11,44 +11,49 @@
 #include "driver.h"
 #include "shared.h"
 
-#define ROTATION_LEEWAY 0.5f
+#define ROTATION_LEEWAY 0.2f
+#define TURN_SPEED 30
+#define FORWARD_SPEED 50
 
 struct waypoint waypoints[] = {
 
-{ .x = 0, .y = 100, .rotation = 6.28f }
+{ .x = 0, .y = 1000, .rotation = 6.28f }
 
 };
 int currentTask = 0;
 
 void driver(void*ignore) {
+	//printf("Driver Task Started \n");
 	struct Particle location = getWeightedAverage();
 	float rotationNeeded = calculateRotation(&location,
 			&waypoints[currentTask]);
+	printf("Rotation Needed : %f \n",rotationNeeded);
+
 	if (fabsf(rotationNeeded) > ROTATION_LEEWAY) {
 		setRotation(rotationNeeded);
 	} else {
 		setMovement();
 	}
 	//checkComlpeted(&waypoints[currentTask]);
-	delay(20);
+	delay(50);
 }
 
 float calculateRotation(struct Particle * currentLocation,
 		struct waypoint * goalLocation) {
 	int dY = goalLocation->y - currentLocation->y;
 	int dX = goalLocation->x - currentLocation->x;
-	return (PI / 2 - atan2(dY, dX)) + currentLocation->heading;
+	return (PI / 2 - atan2(dY, dX) - currentLocation->heading);
 }
 
 void setMovement() {
-	setMotors(50, 50);
+	setMotors(FORWARD_SPEED, FORWARD_SPEED);
 }
 void setRotation(float rotationNeeded) {
 	printf("Required Rotation: %f \n", rotationNeeded);
 	if (rotationNeeded < 0) {
-		setMotors(-50, 50);
+		setMotors(-TURN_SPEED, TURN_SPEED);
 	} else {
-		setMotors(50, -50);
+		setMotors(TURN_SPEED, -TURN_SPEED);
 	}
 }
 
